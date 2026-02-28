cae088605b4a9684d5d74df27886b6165ab8180e
carlos <carlos.avilam@my.unitec.edu.mx>
2026-02-27 21:33:31 -0600

    Se agrega la logica a basic-page para mostrar como funcionan los pipes basicos de angular

--- src/app/pages/basic-page/basic-page.ts
+++ src/app/pages/basic-page/basic-page.ts
@@ -1,8 +1,17 @@
-import {  Component } from '@angular/core';
+import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import {  Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [],
+  imports: [
+    LowerCasePipe, UpperCasePipe, TitleCasePipe
+  ],
   templateUrl: './basic-page.html',
 })
-export  default class BasicPage { }
+export  default class BasicPage { 
+
+  nameLower= signal('angular');
+  nameUpper = signal('ANGULAR');
+  fullName = signal('AnGulAr FraMewOrK');
+
+}
