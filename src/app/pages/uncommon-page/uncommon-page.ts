cbc4e2d8a6a727ffbe47cd9805ef5f57c11d9029
carlos <carlos.avilam@my.unitec.edu.mx>
2026-03-09 21:13:46 -0600

    Se agrega la configuracion con el pipe de Angular i18nSelectPipe al componente uncommon-page.ts

--- src/app/pages/uncommon-page/uncommon-page.ts
+++ src/app/pages/uncommon-page/uncommon-page.ts
@@ -1,11 +1,45 @@
+import { I18nSelectPipe } from '@angular/common';
 import { Card } from './../../components/card/card';
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+
+
+const client1 = {
+  name : 'John Doe',
+  gender: 'male',
+  age: '30',
+  address:'Ottawa, Canada',
+};
+
+const client2 = {
+  name : 'Jane Smith',
+  gender: 'female',
+  age: '25',
+  address:'Toronto, Canada',
+}
 
 @Component({
   selector: 'app-uncommon-page',
   imports: [
-    Card
+    Card, I18nSelectPipe
   ],
   templateUrl: './uncommon-page.html',
 })
-export  default class UncommonPage { }
+export  default class UncommonPage {
+
+  //i18n Select
+  client = signal(client1);
+
+  invitationMap = {
+    male : 'invitarlo',
+    female : 'invitarla',
+  }
+
+  changeClient(){
+    if(this.client() === client1){
+      this.client.set(client2);
+      return;
+    }
+    this.client.set(client1);
+  }
+
+ }
